1b065c197c4e5d6416dfddbe904a718614c5c0be
Urmas K <urmas.kryner@gmail.com>
2023-11-27 22:51:03 +0000

    got squared brackets back

--- final_project/router/booksdb.js
+++ final_project/router/booksdb.js
@@ -1,14 +1,14 @@
 let books = {
-      "9781603095263": {"author": "Chinua Achebe","title": "Things Fall Apart", "reviews": "" },
-      "9881603095274": {"author": "Hans Christian Andersen","title": "Fairy tales", "reviews": "" },
-      "9783603095273": {"author": "Dante Alighieri","title": "The Divine Comedy", "reviews": "" },
-      "9781603195273": {"author": "Unknown","title": "The Epic Of Gilgamesh", "reviews": "" },
-      "9781603095273": {"author": "Unknown","title": "The Book Of Job", "reviews": "" },
-      "9781603094273": {"author": "Unknown","title": "One Thousand and One Nights", "reviews": "" },
-      "9781603095273": {"author": "Unknown","title": "Njl's Saga", "reviews": "" },
-      "9781602095273": {"author": "Jane Austen","title": "Pride and Prejudice", "reviews": "" },
-      "9782605095273": {"author": "Honor de Balzac","title": "Le Pre Goriot", "reviews": "" },
-      "9784603095273": {"author": "Samuel Beckett","title": "Molloy, Malone Dies, The Unnamable, the trilogy", "reviews": "" }
+      "9781603095263": {"author": "Chinua Achebe","title": "Things Fall Apart", "reviews": [] },
+      "9881603095274": {"author": "Hans Christian Andersen","title": "Fairy tales", "reviews": [] },
+      "9783603095273": {"author": "Dante Alighieri","title": "The Divine Comedy", "reviews": [] },
+      "9781603195273": {"author": "Unknown","title": "The Epic Of Gilgamesh", "reviews": [] },
+      "9781603095273": {"author": "Unknown","title": "The Book Of Job", "reviews": [] },
+      "9781603094273": {"author": "Unknown","title": "One Thousand and One Nights", "reviews": [] },
+      "9781603095273": {"author": "Unknown","title": "Njl's Saga", "reviews": [] },
+      "9781602095273": {"author": "Jane Austen","title": "Pride and Prejudice", "reviews": [] },
+      "9782605095273": {"author": "Honor de Balzac","title": "Le Pre Goriot", "reviews": [] },
+      "9784603095273": {"author": "Samuel Beckett","title": "Molloy, Malone Dies, The Unnamable, the trilogy", "reviews": [] }
 };
 
 module.exports=books;
